6e6d9dfd96c2e9084a3130e06c4ce4fce97f91a5
devAndreFC <dev.andrefc@gmail.com>
2023-05-04 17:14:36 -0300

    exibe-dados-clientes-cadastrados

--- clientes/urls.py
+++ clientes/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.clientes, name='clientes')
+    path('', views.clientes, name='clientes'),
+    path('atualiza_cliente/', views.att_cliente, name='atualiza_cliente'),
 ]
--- templates/static/clientes/js/clientes.js
+++ templates/static/clientes/js/clientes.js
@@ -1,24 +1,56 @@
 function add_carro() {
-  container = document.getElementById("form-carro")
+  container = document.getElementById("form-carro");
 
   html =
-    "<br>  <div class='row'> <div class='col-md'> <input type='text' placeholder='carro' class='form-control' name='carro' > </div> <div class='col-md'><input type='text' placeholder='Placa' class='form-control' name='placa' ></div> <div class='col-md'> <input type='date' placeholder='ano' class='form-control' name='ano'> </div> </div>"
+    "<br>  <div class='row'> <div class='col-md'> <input type='text' placeholder='carro' class='form-control' name='carro' > </div> <div class='col-md'><input type='text' placeholder='Placa' class='form-control' name='placa' ></div> <div class='col-md'> <input type='date' placeholder='ano' class='form-control' name='ano'> </div> </div>";
 
-  container.innerHTML += html
+  container.innerHTML += html;
 }
 
-function exibir_form(tipo){
+function exibir_form(tipo) {
+  add_cliente = document.getElementById("adicionar-cliente");
+  att_cliente = document.getElementById("att_cliente");
 
-    add_cliente = document.getElementById("adicionar-cliente")
-    att_cliente = document.getElementById("att_cliente")
+  if (tipo == "1") {
+    att_cliente.style.display = "none";
+    add_cliente.style.display = "block";
+  } else if (tipo == "2") {
+    add_cliente.style.display = "none";
+    att_cliente.style.display = "block";
+  }
+}
 
-    if(tipo == '1'){
-        att_cliente.style.display = "none"
-        add_cliente.style.display = "block"
+function dados_cliente(){
+  cliente = document.getElementById('cliente-select')
+  csrf_token = document.querySelector('[name=csrfmiddlewaretoken]').value
+  id_cliente = cliente.value
 
-    }else if(tipo == '2'){
-        add_cliente.style.display = "none"
-        att_cliente.style.display = "block"
-    }
+  data = new FormData()
+  data.append('id_cliente', id_cliente)
 
+  fetch("/clientes/atualiza_cliente/",{
+      method: "POST",
+      headers: {
+          'X-CSRFToken': csrf_token,
+      },
+      body: data
+
+  }).then(function(result){
+      return result.json()
+  }).then(function(data){
+    document.getElementById('form-att-cliente').style.display = 'block'
+    console.log(data)
+    nome = document.getElementById('nome')
+    nome.value = data['nome']
+
+    sobrenome = document.getElementById('sobrenome')
+    sobrenome.value = data['sobrenome']
+
+    cpf = document.getElementById('cpf')
+    cpf.value = data['cpf']
+
+    email = document.getElementById('email')
+    email.value = data['email']
+
+    })
 }
